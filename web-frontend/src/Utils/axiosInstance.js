456053e676fbf0f27b61c5366c2d97a15c385f23
mcodog <markandrie.codog@tup.edu.ph>
2025-03-08 09:30:03 +0800

    fix

--- web-frontend/src/Utils/axiosInstance.js
+++ web-frontend/src/Utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 // const API_BASE_URL = "http://localhost:8000/";
-const API_BASE_URL = "http://192.168.166.101:8000/";
+const API_BASE_URL = "http://192.168.177.101:8000/";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
